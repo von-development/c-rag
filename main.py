be6c9cd54256ca49bd2701269c2539c9841d67f9
Eden Marco <emarco177@gmail.com>
2024-05-13 16:37:41 +0300

    boilerplate setup

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 from dotenv import load_dotenv
 
 load_dotenv()
-from pprint import pprint
-
-from graph.graph import app
-
-question1 = "What are the types of agent memory?"
-question2 = "How does the AlphaCodium paper work?"
-inputs = {"question": question1}
-
-for output in app.stream(inputs):
-    for key, value in output.items():
-        pprint(f"Finished running: {key}:")
-pprint(value["generation"])
+if __name__ == "__main__":
+    print("Hello Advanced RAG")
